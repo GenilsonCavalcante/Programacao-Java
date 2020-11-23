0748c421c6148ada999be8c98ba0a3c1b6bbbb92
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-11-23 09:06:15 -0300

    Foi adicionado o arquivo "Aula041"

---
+++ Aula041/src/aula41/PalavrasReservadas.java
@@ -0,0 +1,10 @@
+package aula41;
+
+public class PalavrasReservadas {
+	public static void main(String[] args) {
+		
+		//São 52 palavras reservadas para diversar finalidades pro java
+		//EX: while; for; finally; int; public; interface...
+	}
+
+}
