ee0f90872d3162f334ef2e73329141d2463073e8
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-11-23 09:20:33 -0300

    Foi adicionado o arquivo "Aula042"

---
+++ Aula042/src/aula42/IntroducaoAosArrays.java
@@ -0,0 +1,26 @@
+package aula42;
+
+public class IntroducaoAosArrays {
+	public static void main(String[] args) {
+		
+		/*
+		 * Array é uma estrutura de dados que permite a manipulação de um
+		 * número finito de elementos de um mesmo tipo.
+		 * No java, Array é uma coleção ordenada de elementos.
+		 * 
+		 * Em java os tipos de dados se dividem em 2 categorias:
+		 * 1) Tipos primitivos
+		 * 2) Tipos de referência
+		 * 
+		 * Com os Array's, podemos manipular grandes quantidades de dados,
+		 * é uma das partes muito importantes para entender a lógica da programação
+		 * 
+		 * Temos que em um array possuimos os elementos e cada elementos é denominado
+		 * por índices que indicam as suas posições, começado sempre pelo índice 0.
+		 */
+		//Exemplo de declaração de um Array:
+		
+		int[] arrInt = new int[5];
+	}
+
+}
