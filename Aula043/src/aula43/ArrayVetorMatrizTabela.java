e8a0777e8df88af7a460e8b73d149fff2f545fb0
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-11-23 09:41:09 -0300

    Foi adicionado o arquivo 'Aula043'

---
+++ Aula043/src/aula43/ArrayVetorMatrizTabela.java
@@ -0,0 +1,30 @@
+package aula43;
+
+public class ArrayVetorMatrizTabela {
+	public static void main(String[] args) {
+		
+		/* SINÔNIMOS:
+		 * 
+		 * Array - Palavra do inglês que significa "Arranjo"
+		 * Vetor - Array de uma única dimensão
+		 * Tabela - Array com 2 dimensões(array bidimensional) - Possui linhas e colunas.
+		 * Matriz - Array com 'n' dimensões.
+		 * 
+		 * Ou seja, todas essas nomeclaturas são sinônimos, e siginificam a mesma coisa.
+		 */
+		
+		/*
+		 * EXEMPLOS:
+		 * -É utilizado Array para calcular os pixels de uma tela, através das coordenadas x e y.
+		 */
+		
+		/*
+		 * ARRAY MULTIDIMENSIONAL:
+		 * 
+		 * Podemos declarar arrays com quantas dimensões forem necessárias. A quantidadede elementos
+		 * em um Array é limitada pela quantidade de memórias disponível no computador a ser executado.
+		 */
+		int[][] tabela = new int[7][4];
+	}
+
+}
