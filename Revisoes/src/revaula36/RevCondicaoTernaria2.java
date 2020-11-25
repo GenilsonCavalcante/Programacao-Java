f7e28c2eaaa586e10eb92979e69a689f82eeeb9f
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-11-25 16:20:12 -0300

    Revisando-Condicao-Ternaria2-com-o-switch-case

--- Revisoes/src/revaula36/RevCondicaoTernaria2.java
+++ Revisoes/src/revaula36/RevCondicaoTernaria2.java
@@ -9,8 +9,24 @@ public class RevCondicaoTernaria2 {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		
-		String num = (n == 1) ? "Número Digitado: " + n : (n == 2) ? "Número Digitado: " + n : (n == 3) ? "Número Digitado: " + n : (n == 4) ? "Número Digitado: " + n : (n == 5) ? "Número Digitado: " + n : (n == 6)  ? "Número Digitado: " + n : (n == 7) ? "Número Digitado: " + n : (n == 8) ? "Número Digitado: " + n : (n == 9) ? "Número Digitado: " + n : (n == 10) ? "Número Digitado: " + n : "Número Inválido";
-		System.out.println(num);
+//		String num = (n == 1) ? "Número Digitado: " + n : (n == 2) ? "Número Digitado: " + n : (n == 3) ? "Número Digitado: " + n : (n == 4) ? "Número Digitado: " + n : (n == 5) ? "Número Digitado: " + n : (n == 6)  ? "Número Digitado: " + n : (n == 7) ? "Número Digitado: " + n : (n == 8) ? "Número Digitado: " + n : (n == 9) ? "Número Digitado: " + n : (n == 10) ? "Número Digitado: " + n : "Número Inválido";
+//		System.out.println(num);
+		
+		String numero;
+		switch(n) {
+		case 1 : numero = "Número Digitado: " + n;break;
+		case 2 : numero = "Número Digitado: " + n;break;
+		case 3 : numero = "Número Digitado: " + n;break;
+		case 4 : numero = "Número Digitado: " + n;break;
+		case 5 : numero = "Número Digitado: " + n;break;
+		case 6 : numero = "Número Digitado: " + n;break;
+		case 7 : numero = "Número Digitado: " + n;break;
+		case 8 : numero = "Número Digitado: " + n;break;
+		case 9 : numero = "Número Digitado: " + n;break;
+		case 10 : numero = "Número Digitado: " + n;break;
+		default : numero = "Número Inválido!!!";
+		}
+		System.out.println(numero);
 	}
 
 }
