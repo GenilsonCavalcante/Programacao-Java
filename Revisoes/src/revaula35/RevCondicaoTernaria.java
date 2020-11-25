e558ec812cf5cae688e9dfd755dc98355d0ec448
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-11-25 07:30:12 -0300

    Revisando-a-aula035-condição-ternária

---
+++ Revisoes/src/revaula35/RevCondicaoTernaria.java
@@ -0,0 +1,23 @@
+package revaula35;
+
+import java.util.Scanner;
+
+public class RevCondicaoTernaria {
+	public static void main(String[] args) {
+		
+		//Revisão da aula035 - Condição Ternária 1
+		
+		/*
+		 * Com a condição ternária, podemos fazer o if e o else em apenas uma linha de código
+		 */
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Digite um número inteiro: ");
+		int n = in.nextInt();
+		String verificacao = (n%2==0) ? "Número PAR" : "Número IMPAR";
+		System.out.println(verificacao);
+		
+		
+	}
+
+}
