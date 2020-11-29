29abe22220365414dd0b14b887ac978e7e3810d3
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-11-29 11:13:56 -0300

    Adicionado o arquivo Aula049

---
+++ Aula049/src/aula49/RevisaoDeArraysUnidimensionais.java
@@ -0,0 +1,10 @@
+package aula49;
+
+public class RevisaoDeArraysUnidimensionais {
+	public static void main(String[] args) {
+		
+		String[] meuArray = new String[5];
+		//A variável meuArray é uma referência (instância) a um objeto Array.
+	}
+
+}
