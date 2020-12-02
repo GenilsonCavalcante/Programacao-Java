3a9268bf56ac45a8278113bb6d793ac66260f1f2
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-12-02 13:20:16 -0300

    Testando Divisões para resultado igual á 0

---
+++ MatematicaIndeterminada/src/teste/Matematics.java
@@ -0,0 +1,15 @@
+package teste;
+import java.util.Scanner;
+public class Matematics {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		double x, y, divisao, inverso;
+		x = in.nextInt();
+		y = in.nextInt();
+		System.out.println(inverso = ((x/y) * y) % x);
+
+	}
+
+}
