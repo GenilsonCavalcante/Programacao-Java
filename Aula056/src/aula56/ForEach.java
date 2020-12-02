352c02303522580881bb01f98656db0fda9a69e3
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-12-02 16:56:41 -0300

    Add Aula056

---
+++ Aula056/src/aula56/ForEach.java
@@ -0,0 +1,15 @@
+package aula56;
+
+public class ForEach {
+	public static void main(String[] args) {
+		
+		/*
+		 * For Each(Para Cada) é uma estrutura de repetição especializada.
+		 * 
+		 * for( <variavel> : <array> )
+		 * 
+		 * for( int item : lista )
+		 */
+	}
+
+}
